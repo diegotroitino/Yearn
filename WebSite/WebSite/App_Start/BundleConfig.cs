e130f365c52f647d8a93e224aaa71b10561efd2d
reinaldoarrosi <rarrosi@gmail.com>
2013-10-07 16:04:08 -0300

    initial commit

---
+++ WebSite/WebSite/App_Start/BundleConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace WebSite.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/js/core.js").Include(
+                "~/Content/scripts/libs/jquery-{version}.js",
+                "~/Content/scripts/libs/bootstrap.js"
+                )
+            );
+
+            bundles.Add(new StyleBundle("~/bundles/css/core.css").Include(
+                "~/Content/css/bootstrap.css",
+                "~/Content/css/font-awesome.css"
+                )
+            );
+        }
+    }
+}
